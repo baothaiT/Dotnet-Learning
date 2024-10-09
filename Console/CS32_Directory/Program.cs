990b3c01b3ba5ba89aefc071afa5a1c455026be1
DESKTOP-D1B32EL\bao <thaibao0225@gmail.com>
2024-10-09 17:47:06 +0700

    Create new learning with Directory

---
+++ Console/CS32_Directory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace CS32_Directory;
+
+public class Program{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, CS32_Directory!");
+
+        Console.WriteLine("Path: " + Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+        var currentDirectory = System.IO.Directory.GetCurrentDirectory(); 
+        Console.WriteLine("Path2 : " + currentDirectory);
+
+        var parentDirectory = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
+        
+        Console.WriteLine("Target Path: " + parentDirectory);
+    }
+}
+
