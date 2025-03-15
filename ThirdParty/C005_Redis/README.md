5d23b4ddcdf56e599822c437660056d8ebf81bb7
baott <baott@tinhvan.com>
2025-03-15 23:34:56 +0700

    Add redis project

---
+++ ThirdParty/C005_Redis/README.md
@@ -0,0 +1,12 @@
+#
+
+
+##
+
+
+## Packages
+dotnet add package StackExchange.Redis
+
+
+## Docker 
+docker run --name redis -d -p 6379:6379 redis
---
+++ ThirdParty/C005_Redis/WebAPI/Controllers/RedisController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace WebAPI.Controllers
+{
+    public class RedisController : Controller
+    {
+        private readonly IDatabase _database;
+
+        public RedisController(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+        [HttpPost("set")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            await _database.StringSetAsync(key, value);
+            return Ok($"Key '{key}' set successfully!");
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            var value = await _database.StringGetAsync(key);
+            if (value.IsNullOrEmpty)
+                return NotFound("Key not found");
+
+            return Ok(value.ToString());
+        }
+    }
+}
