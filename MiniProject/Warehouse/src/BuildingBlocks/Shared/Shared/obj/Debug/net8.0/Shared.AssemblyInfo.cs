12c4a07404648d26a22b94be9fbc88d92101461e
baott <baott@tinhvan.com>
2025-04-12 20:07:42 +0700

    update Warehouse

--- MiniProject/Warehouse/src/BuildingBlocks/Shared/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ MiniProject/Warehouse/src/BuildingBlocks/Shared/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e47a3e05b007d5311b71f23dbe6c9bb766112d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db3ce444e79fe1d6486e6b0ab245ecd3a62af6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MiniProject/Warehouse/src/User/User.Persistence/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace User.Persistence
+{
+    public class Class1
+    {
+
+    }
+}
