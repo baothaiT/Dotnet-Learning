da1824e3cdbe75c975c4f2ccb5126ed48208eea1
baott <baott@tinhvan.com>
2025-08-02 21:37:14 +0700

    update code

--- DBContext/SQLContext/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace C001_SQLContext.Entities;
-
-public class ProductEntity
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int Price { get; set; }
-    public int Stock { get; set; }
-}
